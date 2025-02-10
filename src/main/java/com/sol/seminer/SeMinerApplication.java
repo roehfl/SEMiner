11d897c7096c67251a5afaf6ea580c1c54ed77cf
roehfl <roehfl@gmail.com>
2025-02-10 09:43:45 +0900

    Initial commit

---
+++ src/main/java/com/sol/seminer/SeMinerApplication.java
@@ -0,0 +1,13 @@
+package com.sol.seminer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeMinerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeMinerApplication.class, args);
+    }
+
+}
